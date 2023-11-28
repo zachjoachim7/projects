d352e1582738cafd48382eeb8cf0b4e4f2cdbc0c
zachjoachim7 <zachjoachim@verizon.net>
2023-11-28 10:00:54 -0700

    Created ServerFacade for the Client to interact with

--- ChessPhase1/client/src/main/ui/Client.java
+++ ChessPhase1/client/src/main/ui/Client.java
@@ -1,7 +1,6 @@
 package ui;
 
 import java.util.Scanner;
-import ui.BoardPrinter;
 
 public class Client {
 
@@ -41,29 +40,66 @@ public class Client {
         String firstPart = brokenDownCommand[0].toLowerCase();
         switch (firstPart) {
             case "register":
+                if (brokenDownCommand.length == 4) {
+                    register(brokenDownCommand[1], brokenDownCommand[2], brokenDownCommand[3], loggedIn);
+                }
+                else {
+                    System.out.println("Needs more arguments");
+                }
                 break;
             case "login":
+                if (brokenDownCommand.length == 3) {
+                    login(brokenDownCommand[1], brokenDownCommand[2], isLoggedIn);
+                }
+                else {
+                    System.out.println("Needs more arguments");
+                }
                 break;
             case "help":
                 help(loggedIn);
                 break;
             case "create":
-                if (brokenDownCommand[1].equals("")) {
-                    System.out.println("You need a game name");
-                    break;
+                if (brokenDownCommand.length == 2) {
+                    createGame(brokenDownCommand[1], isLoggedIn);
                 }
-                createGame(brokenDownCommand[1]);
+                else {
+                    System.out.println("Need a game name");
+                }
                 break;
             case "join":
+                if (brokenDownCommand.length == 3) {
+                    joinGame(brokenDownCommand[1], brokenDownCommand[2], isLoggedIn);
+                }
+                else {
+                    System.out.println("Invalid Arguments");
+                }
                 break;
             case "quit":
                 quit();
                 break;
             case "list":
+                if (brokenDownCommand.length == 1) {
+                    list(isLoggedIn);
+                }
+                else {
+                    System.out.println("Invalid Arguments");
+                }
                 break;
             case "observe":
+                if (brokenDownCommand.length == 2) {
+                    observeGame(brokenDownCommand[1], isLoggedIn);
+                }
+                else {
+                    System.out.println("Invalid Arguments");
+                }
                 break;
             case "logout":
+                if (brokenDownCommand.length == 1) {
+                    logout(isLoggedIn);
+                }
+                else {
+                    System.out.println("Invalid Arguments");
+                }
                 break;
         }
 
@@ -94,38 +130,45 @@ public class Client {
         System.out.println("Registering user: " + username);
     }
 
-    private void login(String username, String password) {
+    private void login(String username, String password, boolean loggedIn) {
         // TODO: Implement login logic
         //isLoggedIn = true;
         System.out.println("User logged in: " + username);
     }
 
-    private void printHelp() {
-        // TODO: Print out help information
-        System.out.println("Available commands: register, login, help, create, join, observe, logout, quit");
-    }
-
-    private void createGame(String gameName) {
+    private void createGame(String gameName, boolean loggedIn) {
         String[][] initialBoard = {
-                {"\u001b[;100;m 1", "R", "N", "B", "Q", "K", "B", "N", "R", "\u001b[;100;m 1"},
-                {"\u001b[;100;m 2", "P", "P", "P", "P", "P", "P", "P", "P", "\u001b[;100;m 2"},
-                {"\u001b[;100;m 3", " ", " ", " ", " ", " ", " ", " ", " ", "\u001b[;100;m 3"},
-                {"\u001b[;100;m 4", " ", " ", " ", " ", " ", " ", " ", " ", "\u001b[;100;m 4"},
-                {"\u001b[;100;m 5", " ", " ", " ", " ", " ", " ", " ", " ", "\u001b[;100;m 5"},
-                {"\u001b[;100;m 6", " ", " ", " ", " ", " ", " ", " ", " ", "\u001b[;100;m 6"},
-                {"\u001b[;100;m 7", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u001b[;100;m 7"},
-                {"\u001b[;100;m 8", "\033[31;40;m \u265C", "\033[31;47;m \u265E", "\033[31;40;m \u265D", "\033[31;47;m \u265B",
-                        "\033[31;40;m \u265A", "\033[31;47;m \u265D", "\033[31;40;m \u265E", "\033[31;47;m \u265C", "\u001b[;100;m 8"}
+                {"\u001b[;100;1m ", "\u001b[30;100;1m  h", "\u001b[30;100;1m g", "\u001b[30;100;1m f", "\u001b[30;100;1m e",
+                        "\u001b[30;100;1m d", "\u001b[30;100;1m c", "\u001b[30;100;1m b", "\u001b[30;100;1m a", "\u001b[;100;1m "},
+                {"\u001b[;100;1m 1", "\033[32;47;1m \u265C", "\033[32;40;1m \u265E", "\033[32;47;1m \u265D", "\033[32;40;1m \u265A",
+                        "\033[32;47;1m \u265B", "\033[32;40;1m \u265D", "\033[32;47;1m \u265E", "\033[32;40;1m \u265C", "\u001b[;100;1m 1"},
+                {"\u001b[;100;1m 2", "\033[32;40;1m \u265F", "\033[32;47;1m \u265F", "\033[32;40;1m \u265F", "\033[32;47;1m \u265F",
+                        "\033[32;40;1m \u265F", "\033[32;47;1m \u265F", "\033[32;40;1m \u265F", "\033[32;47;1m \u265F", "\u001b[;100;1m 2"},
+                {"\u001b[;100;1m 3", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ",
+                        "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\u001b[;100;1m 3"},
+                {"\u001b[;100;1m 4", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ",
+                        "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\u001b[;100;1m 4"},
+                {"\u001b[;100;1m 5", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ",
+                        "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\u001b[;100;1m 5"},
+                {"\u001b[;100;1m 6", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\033[;40;1m  ",
+                        "\033[;47;1m  ", "\033[;40;1m  ", "\033[;47;1m  ", "\u001b[;100;1m 6"},
+                {"\u001b[;100;1m 7", "\033[31;47;1m \u265F", "\033[31;40;1m \u265F", "\033[31;47;1m \u265F", "\033[31;40;1m \u265F",
+                        "\033[31;47;1m \u265F", "\033[31;40;1m \u265F", "\033[31;47;1m \u265F", "\033[31;40;1m \u265F", "\u001b[;100;1m 7"},
+                {"\u001b[;100;1m 8", "\033[31;40;1m \u265C", "\033[31;47;1m \u265E", "\033[31;40;1m \u265D", "\033[31;47;1m \u265A",
+                        "\033[31;40;1m \u265B", "\033[31;47;1m \u265D", "\033[31;40;1m \u265E", "\033[31;47;1m \u265C", "\u001b[;100;1m 8"},
+                {"\u001b[;100;1m ", "\u001b[30;100;1m  h", "\u001b[30;100;1m g", "\u001b[30;100;1m f", "\u001b[30;100;1m e",
+                        "\u001b[30;100;1m d", "\u001b[30;100;1m c", "\u001b[30;100;1m b", "\u001b[30;100;1m a", "\u001b[;100;1m "}
         };
-        String[] unicodePieces = {
-                "r", "n", "b", "q", "k", "p", "R", "N", "B", "Q", "K", "P", "."
-        };
-        String[] unicodePiecesCharacters = {
-                "\u265C", "\u265E", "\u265D", "\u265B", "\u265A", "\u265F",
-                "\u2656", "\u2658", "\u2657", "\u2655", "\u2654", "\u2659", " "
-        };
+
         for (int i = 0; i < initialBoard.length; i++) {
             for (int j = 0; j < initialBoard[i].length; j++) {
+                String piece = initialBoard[i][j];
+                System.out.print(piece + " ");
+            }
+            System.out.println();
+        }
+        for (int i = initialBoard.length - 1; i >= 0; i--) {
+            for (int j = initialBoard.length - 1; j >= 0; j--) {
                 String piece = initialBoard[i][j];
                 System.out.print(piece + " ");
             }
@@ -136,17 +179,17 @@ public class Client {
 
 
 
-    private void joinGame(String gameId, String color) {
+    private void joinGame(String gameId, String color, boolean loggedIn) {
         // TODO: Implement game joining logic
         System.out.println("Joining game: " + gameId + " as " + color);
     }
 
-    private void observeGame(String gameId) {
+    private void observeGame(String gameId, boolean loggedIn) {
         // TODO: Implement game observation logic
         System.out.println("Observing game: " + gameId);
     }
 
-    private void logout() {
+    private void logout(boolean loggedIn) {
         // TODO: Implement logout logic
         //isLoggedIn = false;
         System.out.println("User logged out.");
@@ -156,6 +199,10 @@ public class Client {
         // TODO: Implement application quit logic
         System.out.println("Quitting application.");
         System.exit(0); // Terminate the program
+    }
+
+    private void list(boolean loggedIn) {
+
     }
 
 }
